bb9206bf19ea581da81987475719c377362bef36
HO, ICHENG <nasir1023158@gmail.com>
2021-10-21 17:04:51 +0800

    I am an idiot.

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
-console.log("hello world!")
+console.log("This is  a strange and wierd error message.")
+let hurtWord = ["Ouch", "It's hurt!", "I am going to cry", "Why you doing this to me?", "Are we stil friend, Right?","DO NOT DO THIS", "I will revenge"]
 document.getElementById("bt1").addEventListener("click", function(){
-
+    document.getElementById("hw").innerText = hurtWord[getRandomInt(0, hurtWord.length)]
     document.body.style.backgroundColor=getRandomColor()
 })
-
 
 function getRandomColor() {
     var letters = '0123456789ABCDEF';
@@ -13,3 +13,9 @@ function getRandomColor() {
     }
     return color;
   }
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
+}
